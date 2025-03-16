8c74ccba92b6ba7c5075dd3d050b77f293d27376
RafaelTeixeiraMiguel <teixeirarafael12@gmail.com>
2025-03-16 14:23:47 -0300

    Pushing project

---
+++ README.md
@@ -0,0 +1,14 @@
+# Run
+
+Go to the project directory
+
+Run docker compose to start application and database
+
+```bash
+  docker compose up
+```
+
+It will expose the following URLs:
+
+- **API:** [http://localhost:8080](http://localhost:8080/)
+- **Swagger:** [http://localhost:8080/swagger](http://localhost:8080/swagger)
---
+++ src/Ambev.DeveloperEvaluation.Domain/Validation/SaleValidator.cs
@@ -0,0 +1,28 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using FluentValidation;
+
+public class SaleValidator : AbstractValidator<Sale>
+{
+    public SaleValidator()
+    {
+        RuleFor(sale => sale.SaleItems)
+            .Must(NotExceedMaxItemsPerProduct)
+            .WithMessage("Maximum limit of 20 items per product exceeded.");
+    }
+
+    private bool NotExceedMaxItemsPerProduct(List<SaleItem> saleItems)
+    {
+        if (saleItems == null || !saleItems.Any())
+            return true;
+
+        var grouped = saleItems
+            .GroupBy(item => item.ProductName)
+            .Select(group => new
+            {
+                ProductName = group.Key,
+                TotalQuantity = group.Sum(item => item.Quantity)
+            });
+
+        return grouped.All(g => g.TotalQuantity <= 20);
+    }
+}
